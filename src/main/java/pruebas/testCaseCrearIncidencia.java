0bd709dc9111902ad736353b358b84cc6b72f4ea
Lydia Prado Ibáñez <Lydia.Prado@alu.uclm.es>
2018-12-07 21:10:05 +0100

    Test case para pruebas de crear incidencias creado

---
+++ src/main/java/pruebas/testCaseCrearIncidencia.java
@@ -0,0 +1,79 @@
+package pruebas;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class testCaseCrearIncidencia {
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "https://www.katalon.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testUntitledTestCase() throws Exception {
+		driver.get("https://mantenimientoequipo2.herokuapp.com/login");
+		driver.findElement(By.name("crearIncidencia")).click();
+		driver.findElement(By.name("listaTiposIncidencia")).click();
+		new Select(driver.findElement(By.name("listaTiposIncidencia"))).selectByVisibleText("Error de fichaje");
+		driver.findElement(By.name("listaTiposIncidencia")).click();
+		driver.findElement(By.name("textoIncidencia")).click();
+		driver.findElement(By.name("textoIncidencia")).clear();
+		driver.findElement(By.name("textoIncidencia")).sendKeys("Hay un error en mi fichaje del día 3 de Diciembre de 2018");
+		driver.findElement(By.name("Aceptar")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+}
